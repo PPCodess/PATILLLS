a644bcaf15e0d6ff9f2c2a24144aae752b905069
ppcodess <prathameshpatil8933@gmail.com>
2024-05-25 12:32:13 +0530

    Your commit messagedfdfdfd

---
+++ 7_OPerators/operators.java
@@ -0,0 +1,87 @@
+public class operators{
+    public static void main(String[] args) {
+        /* 
+        *Arithmeatic Operators 
+        
+        int a=10;
+        int b=25;
+        int Sum=a+b;
+        int Subtract=a-b;
+        int multi=a*b;
+        int div=a/b;
+        int rem=a%b;
+
+        System.out.println("add ="+ Sum);
+        System.out.println("sub ="+ Subtract);
+        System.out.println("multi ="+ multi);
+        System.out.println("division ="+ div);
+        System.out.println("remainder ="+ rem);
+
+        *Unary operators
+
+        *Increment Operator : ++
+
+        (1)Pre-Increment:(++a)
+        int a=25;
+        int b=++a;
+        System.out.println(a);
+        System.out.println(b);
+
+        pehle value change and assign to a then assign this changed value to b;
+
+        (2)Post-Increment:(++a)
+        int a=25;
+        int b=++a;
+        System.out.println(a);
+        System.out.println(b);
+
+        pehle value use then update the value 
+
+        *Decrement Operator 
+        (1)Pre-Decrement:(--a)
+        int a=30;
+        int b=--a;
+        System.out.println(a);
+        System.out.println(b);
+        29
+        29
+
+        (2)Post-Decrement :(a++)
+        int a=30;
+        int b=a++;
+        System.out.println(a);
+        System.out.println(b);
+        29
+        30
+
+         
+        *Relational Operator
+        int a=25;
+        int b=30;
+        System.out.println(a==b);
+        
+        int a=25;
+        int b=30;
+        System.out.println(a!=b);
+
+        int a=25;
+        int b=30;
+        System.out.println(a>b);
+        
+        int a=25;
+        int b=30;
+        System.out.println(a<b);
+        
+        
+        int a=25;
+        int b=30;
+        System.out.println(a<=b);
+        
+        
+        int a=25;
+        int b=30;
+        System.out.println(a>=b);
+        */
+        
+    }
+}
